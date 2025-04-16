e5c8e99f605989fcf6fa38a2c6c92ef03a4ec418
Smit Amin <smitamin@uvic.ca>
2025-04-15 22:37:47 -0700

    updated requirements, and added initial code to open camera

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 import cv2
-print(cv2.__version__)
+from cvzone.HandTrackingModule import HandDetector
+
+cap = cv2.VideoCapture(0)   #0 is webcam id
+cap.set(3, 1280)    #3-width map, 1280 width
+cap.set(4, 720)     #4-height map, 720 height
+
+while True:
+    #next 3 lines for running webcam
+    success, img = cap.read()
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)  #1ms
